a4b7a76adf83ca9451e4efcd1d27b4756c30d4b0
PraveenRanasinghe <praveenranasinghe43@gmail.com>
2021-08-21 21:56:15 +0530

    Completed Add Classroom

--- src/main/java/com/timetabling/demo/mobile/mobileController/MobileAdminController.java
+++ src/main/java/com/timetabling/demo/mobile/mobileController/MobileAdminController.java
@@ -1,8 +1,11 @@
 package com.timetabling.demo.mobile.mobileController;
 
 
+import com.timetabling.demo.dto.classRoomDTO;
 import com.timetabling.demo.dto.userDTO;
 import com.timetabling.demo.exceptions.UserAlreadyExistsException;
+import com.timetabling.demo.exceptions.classRoomIdExistException;
+import com.timetabling.demo.mobile.mobileModel.ClassroomDto;
 import com.timetabling.demo.mobile.mobileModel.UserDto;
 import com.timetabling.demo.model.Batch;
 import com.timetabling.demo.model.ClassRoom;
@@ -11,9 +14,7 @@ import com.timetabling.demo.model.Timetable;
 import com.timetabling.demo.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -74,7 +75,7 @@ public class MobileAdminController {
 
     @PostMapping("/addStudents")
     public ResponseEntity<?> StudentRegistration(@RequestBody UserDto dto) throws UserAlreadyExistsException {
-        System.out.println(dto);
+
         userDTO userDTO = new userDTO();
         userDTO.setEmail(dto.getEmail());
         userDTO.setfName(dto.getfName());
@@ -84,6 +85,33 @@ public class MobileAdminController {
         userDTO.setContactNumber(dto.getContactNumber());
 
         userService.registerUsers(userDTO);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping("/addLecturers")
+    public ResponseEntity<?> LecturerRegistration(@RequestBody UserDto dto) throws UserAlreadyExistsException {
+
+        userDTO userDTO = new userDTO();
+        userDTO.setEmail(dto.getEmail());
+        userDTO.setfName(dto.getfName());
+        userDTO.setlName(dto.getlName());
+        userDTO.setUserRole(dto.getUserRole());
+        userDTO.setContactNumber(dto.getContactNumber());
+
+        userService.registerUsers(userDTO);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping("/addClassrooms")
+    public ResponseEntity<?> addClassRooms(@RequestBody ClassroomDto dto) throws classRoomIdExistException {
+
+        classRoomDTO classDto = new classRoomDTO();
+        classDto.setClassRoomID(dto.getClassRoomID());
+        classDto.setCapacity(dto.getCapacity());
+        classDto.setAc(dto.getAc());
+        classDto.setSmartBoard(dto.getSmartBoard());
+
+        classRoomService.addClassRoom(classDto);
         return ResponseEntity.ok(dto);
     }
 
